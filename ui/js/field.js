3947111b42fd48f2129d08ec1003028d6a9a30fc
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2021-08-21 22:22:53 +0200

    display centered playing fields

--- ui/js/field.js
+++ ui/js/field.js
@@ -2,10 +2,7 @@ class Field {
   #tile_width;
   #tile_height;
   #scale_factor;
-  #height_shift;
-
-  #width_add;
-  #height_add;
+  #perspective_correction;
 
   #get_level(levelurl) { //{{{
     return new Promise( resolve => {
@@ -17,23 +14,25 @@ class Field {
     });
   } //}}}
 
-  #coordinate_transform(x,y,height) { //{{{
+  #coordinate_transform(x,y) { //{{{
     // for anybody interested: https://en.wikipedia.org/wiki/Rotation_matrix
     let rot = Math.PI/4; // 45 degrees
     let nx = x * Math.cos(rot) - y * Math.sin(rot);
     let ny = x * Math.sin(rot) + y * Math.cos(rot);
-    return [height+nx,ny];
+    return [nx,ny];
   } //}}}
 
-  #get_random(list) {
+  #get_random(list) { //{{{
     return list[Math.floor((Math.random()*list.length))];
-  }
+  } //}}}
 
   #draw_tile(x,y) { //{{{
     let grax = this.#get_random(this.assets.tiles.normal.graphics).clone();
-    let [nx,ny] = this.#coordinate_transform(x,y,this.y);
-    let g1 = $X('<g transform="scale(' + this.#scale_factor + ',' + this.#scale_factor + ') translate(' + (nx * (this.#tile_width + this.#tile_width/2)) + ',' + (ny * (this.#tile_height - this.#tile_height/2)) + ')" class="tile" element-x="' + x + '" element-y="' + y  + '" xmlns="http://www.w3.org/2000/svg"></g>');
-        g1.append(grax);
+    let [nx,ny] = this.#coordinate_transform(x,y);
+    let pos_x = nx * (this.#tile_width + this.#tile_width/2) + (this.y * this.#tile_width) - (this.#tile_width / 2) - (this.y * this.#perspective_correction);
+    let pos_y = ny * (this.#tile_height - this.#tile_height/2);
+    let g1 = $X('<g transform="scale(' + this.#scale_factor + ',' + this.#scale_factor + ') translate(' + pos_x + ',' + pos_y + ')" class="tile" element-x="' + x + '" element-y="' + y  + '" xmlns="http://www.w3.org/2000/svg"></g>');
+    g1.append(grax);
     this.target.append(g1);
   } //}}}
 
@@ -42,19 +41,15 @@ class Field {
     this.target = target;
     this.levelurl = levelurl;
 
-
     this.#tile_width = 57;
     this.#tile_height = 83.5;
     this.#scale_factor = 0.9;
-    this.#height_shift = 10;
+    this.#perspective_correction = 0;
+   } //}}}
 
-    this.#width_add = this.#tile_width + 12;
-    this.#height_add = this.#tile_height + 8;
-   }  //}}}
-
-  async load_level() {
+  async load_level() { //{{{
     let level = await this.#get_level(this.levelurl);
-    let pieces = level.split(/---\s*\r?\n/).map( x => x.trim() );
+    let pieces = level.split(/---\s*\r?\n/);
     if (pieces.length != 6) {
       bunny_say('Uh, oh. Level is no good.');
       return false;
@@ -87,17 +82,27 @@ class Field {
     this.carrots = this.carrots.split('').map(x => x.trim()).filter(x => x !== undefined);
     this.max_score = parseInt(this.max_score);
     return true;
-  }
+  } //}}}
+
   render() {
     let counter = 0;
-    for (let i=0;i<Math.max(this.x,this.y);i++) {
-      for (let j = counter; j > 0; j--) {
-        this.#draw_tile(j,i-j);
+    for (let i=0;i<Math.max(this.x,this.y)*2;i++) {
+      for (let j = counter; j >= 0; j--) {
+        if (j < this.x && (i-j) < this.y) {
+          if (this.tiles[i-j][j] && this.tiles[i-j][j] != ' ') {
+            this.#draw_tile(j,i-j);
+          }
+        }
       }
       counter += 1;
     }
-    let [wid,hei] = this.#coordinate_transform(this.x,this.y,this.y);
-    this.target.attr('height', hei * (this.#tile_height - this.#tile_height/2) * this.#scale_factor);
-    this.target.attr('width', 2 * wid * (this.#tile_width + this.#tile_width/2) * this.#scale_factor);
+    let [wid,_th] = this.#coordinate_transform(this.x,0);
+    let [_tw,hei] = this.#coordinate_transform(this.x,this.y);
+
+    let iw = (wid * (this.#tile_width  + this.#tile_width/2)  + (this.y * this.#tile_width) + this.#tile_width/2  - this.y * this.#perspective_correction + 2) * this.#scale_factor;
+    let ih = (hei * (this.#tile_height - this.#tile_height/2)                               + this.#tile_height/2 - this.y * this.#perspective_correction + 2) * this.#scale_factor;
+
+    this.target.attr('height', 'auto');
+    this.target.attr('viewBox', '0 0 ' + iw + ' ' + ih);
   }
 }
