5edd79f7ce582a93afdce64f297529ada7cc92f6
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2021-09-14 00:29:03 +0200

    cleanup

--- ui/js/init.js
+++ ui/js/init.js
@@ -10,37 +10,37 @@ document.addEventListener('contextmenu', event => event.preventDefault())
 
 $(document).ready(async function() {
 
-  let assets  = new Assets;
-  await assets.load();
+  let assets  = new Assets
+  await assets.load()
 
-  let q = $.parseQuerySimple();
-  let level = q.level ? q.level : '';
+  let q = $.parseQuerySimple()
+  let level = q.level ? q.level : ''
 
-  let editor = new Editor($('div.program svg'), assets);
-  editor.render();
+  let editor = new Editor($('div.program svg'), assets)
+  editor.render()
 
-  let field = new Field($('div.field svg'), assets, level);
-  if (!(await field.load_level())) { return; }
-  field.render();
+  let field = new Field($('div.field svg'), assets, level)
+  if (!(await field.load_level())) { return }
+  field.render()
 
   walker = new Walker(assets,editor,field)
 
   // show some elements
   $('div.elements *[data-type]').each((_,ele) => { //{{{
-    let iname = $(ele).attr('data-type');
-    let item = assets.commands[iname];
-    $(ele).attr('title',item.label);
+    let iname = $(ele).attr('data-type')
+    let item = assets.commands[iname]
+    $(ele).attr('title',item.label)
     if (field.elements.length == 0) {
-      $(ele).show();
+      $(ele).show()
     } else {
       if (field.elements.includes(iname)) {
-        $(ele).show();
+        $(ele).show()
       }
     }
     $('div.elements *[data-type=' + iname + ']').click(()=>{
       assets.say(item.desc,'div.speech')
-    });
-  });
+    })
+  })
   if (field.elements.includes('execute')) {
     $('#execute').show()
   }
@@ -54,14 +54,14 @@ $(document).ready(async function() {
       editor.render()
       $('div.elements img[data-type=execute' + pid + ']').show()
     }
-  });
+  })
   //}}}
 
   // order
   assets.say(field.order.trim(),'div.speech')
 
   // set mission and title
-  document.title = field.title.trim();
+  document.title = field.title.trim()
   $('div.field div.mission div.top .order').text(field.order)
   $('div.field div.mission div.text').html(marked(field.mission))
   $('div.field div.mission div.text a[href]').attr('target','_blank')
@@ -76,7 +76,7 @@ $(document).ready(async function() {
 
   // display target tiles
   $('div.program svg').on('mouseover','g[element-group=graph] g[element-type=jump]',(ev)=>{ //{{{
-    let epara = $(ev.currentTarget).attr('element-para');
+    let epara = $(ev.currentTarget).attr('element-para')
     if (epara && epara.match(/^\d+,\d+$/)) {
       let [ox,oy] = epara.split(',')
       $('div.field g.tile').removeClass('active')
@@ -84,7 +84,7 @@ $(document).ready(async function() {
     }
   }) //}}}
   $('div.program svg').on('mouseout','g[element-group=graph] g[element-type=jump]',(ev)=>{ //{{{
-    let epara = $(ev.currentTarget).attr('element-para');
+    let epara = $(ev.currentTarget).attr('element-para')
     if (epara && epara.match(/^\d+,\d+$/)) {
       $('div.field g.tile').removeClass('active')
     }
@@ -103,15 +103,15 @@ $(document).ready(async function() {
     active_del = ''
     $(ev.currentTarget).removeClass('active')
     assets.say_reset('div.speech')
-    return false;
-   }); //}}}
+    return false
+   }) //}}}
 
   // drag on SVG does not work. so we have to be clever monkis
   // foreignObject with div inside and at the end of svg.
   // it catches all mouseclicks :-) read on for the grand finale!
   $('div.field svg').on('dragstart','foreignObject div',(ev)=>{ //{{{
-    var left = $(window).scrollLeft();
-    var top = $(window).scrollTop();
+    var left = $(window).scrollLeft()
+    var top = $(window).scrollTop()
 
     $('div.program svg g[element-group=drop] g[element-type=here]').removeClass('active')
     // what fucking clever shit. we hide the foreignObject that sits on top of
@@ -125,24 +125,24 @@ $(document).ready(async function() {
     if (ot.length == 1) {
       let ox = ot.attr('element-x')
       let oy = ot.attr('element-y')
-      ev.originalEvent.dataTransfer.setData("text/plain",ox+','+oy);
+      ev.originalEvent.dataTransfer.setData("text/plain",ox+','+oy)
 
-      var img = document.createElement("img");
-      img.src = "assets/location.svg";
-      ev.originalEvent.dataTransfer.setDragImage(img, 0, 0);
+      var img = document.createElement("img")
+      img.src = "assets/location.svg"
+      ev.originalEvent.dataTransfer.setDragImage(img, 0, 0)
 
       active_drag_location = ox+','+oy
     } else {
       return false
     }
-   }); //}}}
+   }) //}}}
   $('div.program').on('drop','g[element-type=jump]',(ev)=>{ //{{{
-    ev.preventDefault();
-    ev.stopPropagation();
+    ev.preventDefault()
+    ev.stopPropagation()
     if (ev.originalEvent.dataTransfer.getData("text/plain").match(/^\d+,\d+$/)) {
-      let eid = $(ev.currentTarget).attr('element-id');
-      $(ev.currentTarget).removeClass('active');
-      $(ev.currentTarget).addClass('targeting');
+      let eid = $(ev.currentTarget).attr('element-id')
+      $(ev.currentTarget).removeClass('active')
+      $(ev.currentTarget).addClass('targeting')
       $(ev.currentTarget).attr('element-para',ev.originalEvent.dataTransfer.getData("text/plain"))
       editor.update_item(eid,'target',ev.originalEvent.dataTransfer.getData("text/plain"))
 
@@ -150,21 +150,21 @@ $(document).ready(async function() {
       $('div.field g.tile').removeClass('active')
       $('div.field g.tile[element-x=' + ox + '][element-y=' + oy + ']').addClass('active')
     }
-  }); //}}}
+  }) //}}}
   $('div.program').on('dragover','g[element-type=jump]',(ev)=>{ //{{{
-    ev.preventDefault();
-    ev.stopPropagation();
+    ev.preventDefault()
+    ev.stopPropagation()
     if (ev.originalEvent.dataTransfer.getData("text/plain").match(/^\d+,\d+$/) || active_drag_location != null) {
-      $(ev.currentTarget).addClass('active');
+      $(ev.currentTarget).addClass('active')
     }
-  }); //}}}
+  }) //}}}
   $('div.program').on('dragleave','g[element-type=jump]',(ev)=>{ //{{{
-    ev.preventDefault();
-    ev.stopPropagation();
+    ev.preventDefault()
+    ev.stopPropagation()
     if (ev.originalEvent.dataTransfer.getData("text/plain").match(/^\d+,\d+$/) || active_drag_location != null) {
-      $(ev.currentTarget).removeClass('active');
+      $(ev.currentTarget).removeClass('active')
     }
-  }); //}}}
+  }) //}}}
   $(document).on('dragend',ev=>{ //{{{
     if (active_drag_location == null && active_element_drag == null) {
       assets.say(assets.texts.delete,'div.speech')
@@ -180,28 +180,28 @@ $(document).ready(async function() {
       $('div.program svg g[element-group=drop] g[element-type=here]').removeClass('active')
 
       if (active_del_timeout) {
-        clearTimeout(active_del_timeout);
+        clearTimeout(active_del_timeout)
       }
       if (active_del != '') {
-        $('div.program svg g[element-group=drop] g[element-type=delete][element-id=' + active_del + ']').removeClass('active');
+        $('div.program svg g[element-group=drop] g[element-type=delete][element-id=' + active_del + ']').removeClass('active')
       }
       let eid = ot.first().attr('element-id')
       if (active_del == eid) {
-        active_del = '';
+        active_del = ''
       } else {
-        active_del = eid;
+        active_del = eid
         if (!walker.walking) {
-          $('div.program svg g[element-group=drop] g[element-type=delete][element-id=' + eid + ']').addClass('active');
+          $('div.program svg g[element-group=drop] g[element-type=delete][element-id=' + eid + ']').addClass('active')
         }
-        let iname = ot.first().attr('element-type');
-        let item = assets.commands[iname];
+        let iname = ot.first().attr('element-type')
+        let item = assets.commands[iname]
         assets.say(item.label,'div.speech')
         active_del_timeout = setTimeout(()=>{
-          $('div.program svg g[element-group=drop] g[element-type=delete][element-id].active').removeClass('active');
-          active_del = '';
-        },12000);
+          $('div.program svg g[element-group=drop] g[element-type=delete][element-id].active').removeClass('active')
+          active_del = ''
+        },12000)
       }
-      return false;
+      return false
     }
   }) //}}}
   $('div.program svg').on('dragstart','foreignObject div',(ev)=>{ //{{{
@@ -213,15 +213,15 @@ $(document).ready(async function() {
         return false
       }
       var eid = ot.first().attr('element-id')
-      ev.originalEvent.dataTransfer.setData("text/plain",eid);
+      ev.originalEvent.dataTransfer.setData("text/plain",eid)
 
       let img = $('div.elements img[data-type='+ety+']')[0]
       ev.originalEvent.dataTransfer.setDragImage(img, 0, 0)
 
       $('div.program svg g[element-group=drop] g[element-type=here]').removeClass('active')
       setTimeout(ev=>{ // yes, chrome, you are an idiot
-        $('div.program g[element-type=add] .adder').show();
-      },100);
+        $('div.program g[element-type=add] .adder').show()
+      },100)
       active_element_drag = true
     } else {
       return false
@@ -229,12 +229,12 @@ $(document).ready(async function() {
   }) //}}}
 
   $('div.elements').on('dragstart','[draggable=true][data-type]',(ev)=>{ //{{{
-    ev.originalEvent.dataTransfer.setData("text/plain", $(ev.currentTarget).attr('data-type'));
-    ev.originalEvent.dataTransfer.setDragImage(ev.originalEvent.srcElement, 28, 0);
+    ev.originalEvent.dataTransfer.setData("text/plain", $(ev.currentTarget).attr('data-type'))
+    ev.originalEvent.dataTransfer.setDragImage(ev.originalEvent.srcElement, 28, 0)
     $('div.program svg g[element-group=drop] g[element-type=here]').removeClass('active')
-    $('div.program g[element-type=add] .adder').show();
+    $('div.program g[element-type=add] .adder').show()
     active_element_drag = true
-  }); //}}}
+  }) //}}}
   $('div.program').on('drop','g[element-type=add]',(ev)=>{ //{{{
     ev.preventDefault()
     ev.stopPropagation()
@@ -256,24 +256,24 @@ $(document).ready(async function() {
       active_element_drag = false
       $('div.program g[element-type=add] .adder').hide()
       $('div.program g[element-type=add]').removeClass('active')
-      editor.render();
+      editor.render()
     }
     active_element_drag = false
-  }); //}}}
+  }) //}}}
   $('div.program').on('dragover','g[element-type=add]',(ev)=>{ //{{{
-    ev.preventDefault();
-    ev.stopPropagation();
+    ev.preventDefault()
+    ev.stopPropagation()
     if (ev.originalEvent.dataTransfer.getData("text/plain").match(/^[a-z]{2}[a-z0-9_]+$/) || ev.originalEvent.dataTransfer.getData("text/plain").match(/^a\d+$/) || active_element_drag) {
-      $(ev.currentTarget).addClass('active');
+      $(ev.currentTarget).addClass('active')
     }
-  }); //}}}
+  }) //}}}
   $('div.program').on('dragleave','g[element-type=add]',(ev)=>{ //{{{
-    ev.preventDefault();
-    ev.stopPropagation();
+    ev.preventDefault()
+    ev.stopPropagation()
     if (ev.originalEvent.dataTransfer.getData("text/plain").match(/^[a-z]{2}[a-z0-9_]+$/) || ev.originalEvent.dataTransfer.getData("text/plain").match(/^a\d+$/) || active_element_drag) {
-      $(ev.currentTarget).removeClass('active');
+      $(ev.currentTarget).removeClass('active')
     }
-  }); //}}}
+  }) //}}}
 
   $('button.mission').click(ev=>{
     $('div.field div.victory').removeClass('active')
@@ -295,7 +295,7 @@ $(document).ready(async function() {
       } else {
         walker.walk()
       }
-      $(ev.currentTarget).toggleClass('active');
+      $(ev.currentTarget).toggleClass('active')
     }
   })
   document.addEventListener('cisc:changed', (e) => {
@@ -330,24 +330,24 @@ $(document).ready(async function() {
   })
 
   $('button.save').click(ev=>{
-    $('#saveinstructions').attr('download',field.title.trim().replace(/[^a-zA-Z0-9!?()-]/g,'_') + '.json');
-    $('#saveinstructions').attr('href','data:application/xml;charset=utf-8;base64,' + $B64(JSON.stringify(editor.program,null,2)));
-    document.getElementById('saveinstructions').click();
+    $('#saveinstructions').attr('download',field.title.trim().replace(/[^a-zA-Z0-9!?()-]/g,'_') + '.json')
+    $('#saveinstructions').attr('href','data:application/xml;charset=utf-8;base64,' + $B64(JSON.stringify(editor.program,null,2)))
+    document.getElementById('saveinstructions').click()
   })
   $('button.load').click(ev=>{
-    document.getElementById('loadinstructions').click();
+    document.getElementById('loadinstructions').click()
   })
   $("input[name=loadinstructions]").change(ev=>{
     if (typeof window.FileReader !== 'function') {
-      alert('FileReader not yet supportet');
-      return;
+      alert('FileReader not yet supportet')
+      return
     }
-    var files = $('#loadinstructions').get(0).files;
-    var reader = new FileReader();
+    var files = $('#loadinstructions').get(0).files
+    var reader = new FileReader()
     reader.onload = function(){
       editor.program = JSON.parse(reader.result)
       editor.render()
-      document.getElementById('fuckchrome').reset();
+      document.getElementById('fuckchrome').reset()
       editor.get_pids().forEach(pid => {
         $('div.elements img[data-type=execute' + pid + ']').show()
       })
@@ -355,7 +355,7 @@ $(document).ready(async function() {
     }
     reader.onerror = function(){ console.log('error reading file'); loading = false; }
     reader.onabort = function(){ console.log('abort reading file'); loading = false; }
-    reader.readAsText(files[0]);
+    reader.readAsText(files[0])
 
   })
 })
