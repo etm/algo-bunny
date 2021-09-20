d841a6d8ecfb0aba7a696d76f32b71f70e8b6c54
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2021-09-21 01:58:40 +0200

    print

---
+++ ui/js/print.js
@@ -0,0 +1,33 @@
+document.addEventListener('contextmenu', event => event.preventDefault())
+
+$(document).ready(async function() {
+  let assets  = new Assets
+  await assets.load()
+
+  let editor = new Editor($('div.program svg'), assets)
+  editor.render()
+
+  $('button.load').click(ev=>{
+    document.getElementById('loadinstructions').click()
+  })
+  $("input[name=loadinstructions]").change(ev=>{
+    if (typeof window.FileReader !== 'function') {
+      alert('FileReader not yet supportet')
+      return
+    }
+    var files = $('#loadinstructions').get(0).files
+    var reader = new FileReader()
+    reader.onload = function(){
+      editor.program = JSON.parse(reader.result)
+      editor.render()
+      document.getElementById('fuckchrome').reset()
+      editor.get_pids().forEach(pid => {
+        $('div.elements img[data-type=execute' + pid + ']').show()
+      })
+      $('div.field div.stats .cisc .value').text(editor.cisc_length())
+    }
+    reader.onerror = function(){ console.log('error reading file'); loading = false; }
+    reader.onabort = function(){ console.log('abort reading file'); loading = false; }
+    reader.readAsText(files[0])
+  })
+})
