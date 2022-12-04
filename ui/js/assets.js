08c1b28273a80cf73ed3520b8bdabcc7cf045c82
Juergen 'eTM' Mangler <juergen.mangler@gmail.com>
2022-12-04 07:07:57 +0100

    assets

--- ui/js/assets.js
+++ ui/js/assets.js
@@ -142,32 +142,34 @@ function Assets() {
     "Go ahead, make my day.",
     "Here's looking at you, carrot.",
     "Yippie-ki-yay, carrot.",
-    "I'm the king of the carrots.",
+    "I'm the king of the carrots.", // titanic
     "The greatest teacher, carrot is.",
-    "If you're nothing without the carrot, then you shouldn't have it.",
+    "If you're nothing without the carrot, then you shouldn't have it.", // spider man
     "I am serious, don't call me carrot.",
     "The carrot abides.",
-    "You're gonna need a bigger carrot.",
+    "You're gonna need a bigger carrot.", // jaws
     "May the carrot be with you.",
     "A carrot is a carrot, but they call it 'la carotte'.",
     "Leave the flower. Take the carrots.",
     "Keep your friends close, but your carrots closer.",
-    "This is not 'Nam. This is carrot gathering. There is rules.",
-    "You had me at carrot.",
+    "This is not 'Nom. This is carrot gathering. There is rules.",
+    "You had me at carrot.", // jerry maguire
     "Flowers? Where we're going, we don't need flowers.",
     "I'm having a carrot for dinner.",
     "We'll always have carrots.",
     "I think this is the beginning of a beautiful carrot gathering.",
     "Round up the usual carrots.",
     "Mama always said life is like a carrot. You never know what you're gonna get.",
-    "A carrot, for lack of a better word, is good.",
-    "As God is my witness, I'll never be hungry again.",
+    "A carrot, for lack of a better word, is good.", // wall street (1987)
+    "As God is my witness, I'll never be hungry again.", // gone with the wind
     "Carrots, my dear Watson.",
     "Soylent Orange is carrots.",
     "My carrot!",
     "I feel the need, the need for carrots.",
     "Carpe diem. Seize the carrot. Make your lives extraordinary.",
-    "A million carrots isn't cool. You know what's cool? A billion carrots."
+    "A million carrots isn't cool. You know what's cool? A billion carrots.",
+    "Nobody puts carrot in a corner.", // dirty dancing
+    "You want the carrot? You can't handle the carrot!" // a few good men
   ] //}}}
 
   /*------------------------*/
