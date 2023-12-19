26f208329bfbe2997823e5fb6a76f3581a4abc6c
AsterMiha <chireamihaela99@gmail.com>
2023-12-19 02:29:52 +0100

    Get level data from score server

--- scoreserver/scoreserver.py
+++ scoreserver/scoreserver.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 import os
+import glob
 import json
 import datetime
 
@@ -13,6 +14,13 @@ now = datetime.datetime.now()
 today = now.strftime("%y-%m-%d")
 today = '23-12-17'
 
+levels = [
+    "Carrots!",
+    "Carrots_around_the_corner!",
+    "Carrots_on_a_stick!"
+    ]
+
+
 @app.route('/')
 def hello_world():
     return '<p>Hello, World!</p>'
@@ -22,6 +30,27 @@ def get_students():
     sub_folders = [name for name in os.listdir(folder) if os.path.isdir(os.path.join(folder, name))]
     return sub_folders
 
+def get_student_level_data(student, level = 0):
+    global today
+    
+    if (level == 0):
+        pass # get all levels for the specified student
+    
+    prefix = path_root + 'scores/'
+    score_path = student + '/' + today + "/" + levels[level-1]
+    file_path = prefix + score_path + "*"
+    files = glob.glob(file_path)
+
+    if files == []:
+        return {}
+    # Open last file in list for now
+    latest_sol_file = open(files[-1])
+    latest_sol = json.load(latest_sol_file)
+    latest_sol["sol_source"] = files[-1][len(prefix):]
+
+    return latest_sol
+
+
 @app.route('/init', methods=['GET'])
 def students():
     table_data = {}
@@ -30,7 +59,17 @@ def students():
 
 @app.route('/get/level/<level>', methods=['GET'])
 def get_level_data(level):
-    return 'level: '+level
+    global today, levels
+
+    if (int(level) > len(levels)):
+        return json.dumps({"err": "I don't know that level!"})
+
+    level_data = {"students":{}, "err":"No errors!"}
+    students = get_students()
+    for student in students:
+        level_data["students"][student] = get_student_level_data(student, int(level))
+
+    return json.dumps(level_data)
 
 @app.route('/get/student/<student>', methods=['GET'])
 def get_student_data(student):
--- ui/js/scoreboard.js
+++ ui/js/scoreboard.js
@@ -8,25 +8,50 @@ function add_table_head(name_list) {
     })
 }
 
-function add_row() {
+function insert_next_level() {
+    const rowCount = $("#scoreboard_table tr").length;
+
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:3000/get/level/" + (rowCount - 1),
+        error: () => { console.log("error") }
+        }).then(res => {
+        const response = JSON.parse(res)
+        console.log(response)
+        if ("students" in response) {
+            add_row(response["students"])
+        }
+    })
+}
+
+function add_row(student_data) {
     // Find position to insert row at
     const rowCount = $("#scoreboard_table tr").length;
     const colCount = $("#scoreboard_table tr th").length;
 
+    // Get name order in table header
+    const names = Array.from(document.getElementById("scoreboard_head").children)
+                    .map((cell) => cell.textContent)
+    console.log(names)
+
     let table = document.getElementById("scoreboard_table");
     let row = table.insertRow(rowCount - 1);
 
+    // Create row
     let level_cell = row.insertCell(0);
     level_cell.innerHTML = "New Level!" + (rowCount - 1)
     for (let i = 1; i <= colCount - 1; i++) {
         let cell = row.insertCell(i);
-        cell.innerHTML = i;
+        // Fill cells we have data about
+        let name = names[i]
+        if (name in student_data) {
+            cell.textContent = JSON.stringify(student_data[name]);
+        }
     }
 
     // Change level number next to the button
     let next_level_cell = document.getElementById("next_level_cell");
     next_level_cell.innerHTML = "Next level: " + rowCount
-
 }
 
 $(document).ready(
@@ -44,7 +69,8 @@ $(document).ready(
         btn_add_row.onclick = ((ev)=>{
             console.log(ev)
             console.log("clicked button!")
-            add_row()
+            insert_next_level()
+            // add_row()
         })
     }
 )
